3071336b41fee24bf8774232d8a0cdc13b58b68d
Kamau-sam <skamau.6411@gmail.com>
2024-03-25 13:44:28 +0300

    Minor adjustments and nesting

--- gradeGenerator.js
+++ gradeGenerator.js
@@ -1,28 +1,25 @@
 //The first two lines promts the user to enter their marks
 const prompt = require("prompt-sync")({ sigint: true });
-const marks = prompt("Enter you marks")
+const marks = prompt("Enter you marks");
 
-function getGrade (marks){// function declaration
+function getGrade(marks) {
+  // function declaration
   let grade;
-if (marks > 100 || marks < 0 || isNaN(marks)){
-  grade = `Invalid! Enter numbers between 0-100`
-}//Alert the user that the marks entered are invalid if value entered is below 0 , above 100 or not a number
-else  if (marks > 79){
-  grade = `A`;
-} 
-else if (marks >= 60){
-  grade = `B`;
-}
-else if ( marks >= 49){
-  grade = `C`;
-}
-else if (marks >= 40){
-  grade = `D`;
-}
-else if(marks < 40){
-    grade = `E`
-}
-console.log ("Your grade is:" + grade)
+  if (marks > 100 || marks < 0 || isNaN(marks)) {
+    grade = `Invalid! Enter numbers between 0-100`;
+  } //Alert the user that the marks entered are invalid if value entered is below 0 , above 100 or not a number
+  else if (marks > 79) {
+    grade = `A`;
+  } else if (marks >= 60) {
+    grade = `B`;
+  } else if (marks >= 49) {
+    grade = `C`;
+  } else if (marks >= 40) {
+    grade = `D`;
+  } else if (marks < 40) {
+    grade = `E`;
+  }
+  console.log("Your grade is:" + grade);
 }
 //Means to call the function
 getGrade(marks);
--- salary Calculator.js
+++ salary Calculator.js
@@ -1,3 +1,6 @@
+const prompt = require("prompt-sync")({ sigint: true });
+const salaryInput = prompt("Enter  Salary");
+
 //function declaration
 function NetsalaryCalculator(basicSalary, benefits) {
   // variable declaration/definion
@@ -23,35 +26,35 @@ function NetsalaryCalculator(basicSalary, benefits) {
 
   if (grossSalary <= 5999) {
     nhifDeduction = 150;
-  } else if (grossSalary > 5999 && grossSalary <= 7999) {
+  } else if (grossSalary <= 5999) {
     nhifDeduction = 300;
-  } else if (grossSalary > 7999 && grossSalary <= 11999) {
+  } else if (grossSalary <= 7999) {
     nhifDeduction = 400;
-  } else if (grossSalary > 11999 && grossSalary <= 14999) {
+  } else if (grossSalary <= 11999) {
     nhifDeduction = 500;
-  } else if (grossSalary > 14999 && grossSalary <= 19999) {
+  } else if (grossSalary <= 14999) {
     nhifDeduction = 600;
-  } else if (grossSalary > 19999 && grossSalary <= 24999) {
+  } else if (grossSalary <= 19999) {
     nhifDeduction = 750;
-  } else if (grossSalary > 24999 && grossSalary <= 29999) {
+  } else if (grossSalary <= 24999) {
     nhifDeduction = 850;
-  } else if (grossSalary > 29999 && grossSalary <= 34999) {
+  } else if (grossSalary <= 29999) {
     nhifDeduction = 900;
-  } else if (grossSalary > 34999 && grossSalary <= 39999) {
+  } else if (grossSalary <= 34999) {
     nhifDeduction = 950;
-  } else if (grossSalary > 39999 && grossSalary <= 44999) {
+  } else if (grossSalary <= 39999) {
     nhifDeduction = 1000;
-  } else if (grossSalary > 44999 && grossSalary <= 49999) {
+  } else if (grossSalary <= 44999) {
     nhifDeduction = 1100;
-  } else if (grossSalary > 49999 && grossSalary <= 59999) {
+  } else if (grossSalary <= 49999) {
     nhifDeduction = 1200;
-  } else if (grossSalary > 59999 && grossSalary <= 69999) {
+  } else if (grossSalary <= 59999) {
     nhifDeduction = 1300;
-  } else if (grossSalary > 69999 && grossSalary <= 79999) {
+  } else if (grossSalary <= 69999) {
     nhifDeduction = 1400;
-  } else if (grossSalary > 79999 && grossSalary <= 89999) {
+  } else if (grossSalary <= 79999) {
     nhifDeduction = 1500;
-  } else if (grossSalary > 89999 && grossSalary <= 99999) {
+  } else if (grossSalary <= 89999) {
     nhifDeduction = 1600;
   } else if (grossSalary > 100000) {
     nhifDeduction = 1700;
