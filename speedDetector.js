fd875fcd4d9cd554ead375edccc624cdf084e499
Kamau-sam <skamau.6411@gmail.com>
2024-03-25 13:38:10 +0300

    Code nesting and other minor adjustments

--- speedDetector.js
+++ speedDetector.js
@@ -6,7 +6,7 @@ const speed = Number(speedInput); // Convert the input string to a number
 //Here is the function declaration
 function speedDetector(speed) {
   if (speed <= 70) {
-    console.log("OK"); //Here we print "OK" if the speed is less than 70
+    console.log("OK"); //Here we print "OK" fo speed upto 70
   } else {
     let demeritPoints = Math.floor((speed - 70) / 5); //Here we calculate and allocate 1 demerit point for every 5km above 70
     // Here we print "License suspended" if demeritpoints exceed 12
@@ -16,6 +16,9 @@ function speedDetector(speed) {
     //Here print a message + the demerit points if less than 12
     else {
       console.log("Points: " + demeritPoints);
+    }
+    if (isNaN(speed)) {
+      console.log("Enter a valid Number");
     }
   }
 }
